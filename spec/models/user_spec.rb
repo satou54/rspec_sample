2ad0d14438be14314e9e7310b91f100de1865c5a
satou54 <satou54satou@gmail.com>
2021-01-14 05:35:11 +0900

    userモデルのテストをmergeを使用した形にリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "名前の表示" do
-    let(:params) {{ name: "satou", age: 1 }}
-    let(:params2) {{ name: "satou", age: 15 }}
-    let(:params3) {{ name: "satou", age: 30 }}
+    let(:params) {{ name: "satou" }}
 
    it "年齢1:ユーザの名前が取得できること" do
+      params.merge!(age: 1)
       user = User.new(params)
       expect(user.disp_name).to eq "satouちゃん"
     end
     it "年齢15:ユーザの名前が取得できること" do
-      user = User.new(params2)
+      params.merge!(age: 15)
+      user = User.new(params)
       expect(user.disp_name).to eq "satou君"
     end
     it "年齢20:ユーザの名前が取得できること" do
-      user = User.new(params3)
+      params.merge!(age: 30)
+      user = User.new(params)
       expect(user.disp_name).to eq "satouさん"
     end
   end
